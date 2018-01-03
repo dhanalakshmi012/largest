2189b8ad747ae0d004733cc6a12540233d7adabe
dhanalakshmi012 <33174588+dhanalakshmi012@users.noreply.github.com>
2018-01-03 22:52:56 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,10 @@
+# largest
+num1=raw_input("enter the number:")
+num2=raw_input("enter the number:")
+num3=raw_input("enter the number:")
+if((num1>num2) and (num1>num3)):
+   print("num1 is large")
+elif((num2>num1) and (num2>num3)):
+   print("num2 is large")
+else:
+   print("num3 is large")
